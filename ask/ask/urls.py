71a3b8a0df6ef6a89316d952e56a2443ba954868
Lider-XXI <lider-xxi@mail.ru>
2016-11-19 00:22:46 +0300

    hak for lession 2.7

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -13,16 +13,16 @@ Including another URLconf
     1. Add an import:  from blog import urls as blog_urls
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import include, url
+from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^login/$', 'qa.views.test'),
-    url(r'^signup/$', 'qa.views.test'),
-    url(r'^question/(\d+)$', 'qa.views.test'),
-    url(r'^ask/.*$', 'qa.views.test'),
-    url(r'^popular/$', 'qa.views.test'),
-    url(r'^new/$', 'qa.views.test'),
-]
+urlpatterns = patterns)'qa.views',
+    url(r'^$', 'test'),
+    url(r'^login/.*', 'test', name='login'),
+    url(r'^signup/.*', 'test', name='signup'),
+    url(r'^question/(?P<id>[0-9]+)/$', 'test', name='question'),
+    url(r'^ask/.*', 'test', name='ask'),
+    url(r'^popular/.*', 'test', name='popular'),
+    url(r'^new/.*', 'test', name='new'),
+)
--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def test(request, *args, **kwargs):
-    return HttpResponse('OK')
+    return HttpResponse('OK', status=200)
