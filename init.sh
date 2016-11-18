98b0f275d0fe8baf63bcccadd3d71d65915bab7f
Lider-XXI <lider-xxi@mail.ru>
2016-11-18 22:37:54 +0300

    lession 2.7

--- init.sh
+++ init.sh
@@ -2,4 +2,11 @@ sudo rm /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
-sudo gunicorn -b 0.0.0.0:8080 --workers=9 hello
+sudo pkill gunicorn
+sudo rm /etc/gunicorn.d/*
+sudo ln -s /home/box/web/etc/gunicorn_ask.conf /etc/gunicorn.d/ask
+sudo /etc/init.d/gunicorn restart
+
+
+#gunicorn -b 0.0.0.0:8080 --workers=4 hello &
+#gunikorn -b 0.0.0.0:8000 --workers=4 ask.wsgi &
